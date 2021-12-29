39adf610b3444fdda096c55b696192b1108d3485
KreihsA <akreihs@ascs.pl>
2021-12-29 16:51:05 +0100

    PublicIPCmd timeout support

--- src/main/java/com/ascs/Cmd/PublicIPCmd.java
+++ src/main/java/com/ascs/Cmd/PublicIPCmd.java
@@ -3,14 +3,20 @@ package com.ascs.Cmd;
 import com.ascs.Printer;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Optional;
 
+import static com.ascs.Printer.ERRCOLOR;
 import static com.ascs.Printer.INFOCOLOR;
 
 public class PublicIPCmd implements ICmd {
+
+    static private final int _timeout = 5000;
 
     private final Printer _printer;
 
@@ -25,8 +31,15 @@ public class PublicIPCmd implements ICmd {
     public void exec(Optional<String> arg) throws Exception {
         URL url = new URL("http://ipinfo.io/ip");
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+        try {
+            URLConnection conn = url.openConnection();
+            conn.setReadTimeout(_timeout);
+            conn.setConnectTimeout(_timeout);
+            conn.connect();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
             _printer.println(reader.readLine(), INFOCOLOR);
+        } catch (IOException ex) {
+            _printer.println("Error while getting the public address", ERRCOLOR);
         }
     }
 
